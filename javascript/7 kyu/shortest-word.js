92f9d3aea0f6ebb4a96a8226cb88d5d39c6e21fa
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-06-10 20:24:39 +0200

    Add Shortest word solution

---
+++ javascript/7 kyu/shortest-word.js
@@ -0,0 +1,13 @@
+/* Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+    let arr = [];
+    const words = s.split(" ");
+    for (word of words) {
+        arr.push(word.length);
+    }
+    return arr.sort((a, b) => a - b)[0];
+}
