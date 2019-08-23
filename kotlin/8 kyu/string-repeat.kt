f51e9ad80298bf902a059688add24d14b6d142f2
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-08-23 20:45:56 +0200

    Add opposite number solution

---
+++ kotlin/8 kyu/string-repeat.kt
@@ -0,0 +1,6 @@
+/* Write a function called repeatString which repeats the given String src exactly count times.
+
+repeatstr(6, "I") # "IIIIII"
+repeatstr(5, "Hello") # "HelloHelloHelloHelloHello" */
+
+fun repeatStr(r: Int, str: String) : String = str.repeat(r)
